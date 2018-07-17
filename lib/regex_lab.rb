bbbf978f3c97b73a183b5025eac86752c29f4474
Sharmaine Requerme <srqrme@gmail.com>
2018-07-17 02:26:00 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,31 +1,19 @@
 def starts_with_a_vowel?(word)
-  if word.match(/^[aeiouAEIOU]\w+/)
-    true
-  else
-    false
-  end
+  word.match(/^[aeiouAEIOU]/) ? true : false
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/un\w*ing/)
+def words_starting_with_un_and_ending_with_ing(words_string)
+  words_string.scan(/\w+ing/)
 end
 
-def words_five_letters_long(text)
-  text.scan(/\b\w{5}\b/)
+def words_five_letters_long(words_string)
+  words_string.scan(/\b\w{5}\b/)
 end
 
-def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^[A-Z].+[!?,\."',]$/)
-    true
-  else
-    return false
-  end
+def first_word_capitalized_and_ends_with_punctuation?(correct_punctuation)
+  correct_punctuation.match(/^[A-Z].+[!?,\."',]$/) ? true : false
 end
 
-def valid_phone_number?(phone)
-  if phone.match(/([0-9]{10})|([0-9]{3}) ([0-9]{3}) ([0-9]{4})|\([0-9]{3}\)|([0-9]{7}\b)|([0-9]{3}-[0-9]{4})/)
-    true
-  else
-    return false
-  end
+def valid_phone_number?(number)
+  number.match(/([0-9]{10})|([0-9]{3}) ([0-9]{3}) ([0-9]{4})|\([0-9]{3}\)|([0-9]{7}\b)|([0-9]{3}-[0-9]{4})/) ? true : false
 end
