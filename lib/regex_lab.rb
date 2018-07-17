e7adbc5fdb13bae51c6c2bb6b821af7ef03fc597
Sharmaine Requerme <srqrme@gmail.com>
2018-07-17 01:22:00 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,5 +3,5 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(words_string)
-  words_string.scan(/^un\w+\s\D+ing/)
+  words_string.scan(/^un\w+\s+\D+ing/)
 end
